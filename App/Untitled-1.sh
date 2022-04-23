afbd2d288c9d9ac958f4cbe3fe0c2d79607ea331
unknown <mohsenomar350@gmail.com>
2022-04-23 02:41:25 -0700

    omar

--- App/Untitled-1.sh
+++ App/Untitled-1.sh
@@ -23,19 +23,19 @@ az webapp create --name $SITENAME --plan $PLANNAME --runtime $RUNTIME --resource
 
 # To set up deployment from a local git repository, uncomment the following commands.
 # first, set the username and password (use environment variables!)
-# USERNAME=""
-# PASSWORD=""
-# az webapp deployment user set --user-name $USERNAME --password $PASSWORD
+USERNAME="omarmohsen179"
+PASSWORD="Thanks010066@"
+az webapp deployment user set --user-name $USERNAME --password $PASSWORD
 
 # now, configure the site for deployment. in this case, we will deploy from the local git repository
 # you can also configure your site to be deployed from a remote git repository or set up a CI/CD workflow
-# az webapp deployment source config-local-git --name $SITENAME --resource-group $RESOURCEGROUP
+az webapp deployment source config-local-git --name $SITENAME --resource-group $RESOURCEGROUP
 
 # the previous command returned the git remote to deploy to
 # use this to set up a new remote named "azure"
-# git remote add azure "https://$USERNAME@$SITENAME.scm.azurewebsites.net/$SITENAME.git"
+git remote add azure "https://$USERNAME@$SITENAME.scm.azurewebsites.net/$SITENAME.git"
 # push master to deploy the site
-# git push azure master
+git push azure master
 
 # browse to the site
-# az webapp browse --name $SITENAME --resource-group $RESOURCEGROUP
+az webapp browse --name $SITENAME --resource-group $RESOURCEGROUP
