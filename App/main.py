87c18c12a18832d27d1d4df5681c3a3fe7d3305b
unknown <mohsenomar350@gmail.com>
2022-04-23 02:54:23 -0700

    omar

--- App/main.py
+++ App/main.py
@@ -2,7 +2,7 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from .routes.auth import routerauth
+
 from fastapi.testclient import TestClient
 from fastapi.staticfiles import StaticFiles
 # sqlalchemy uvicorn alembic fastapi pyodbc python-dotenv
@@ -16,8 +16,6 @@ from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
-app.include_router(routerauth)
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
