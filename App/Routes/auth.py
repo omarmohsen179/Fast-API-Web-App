d442f8c9bb3f6654a48c15f6049dc7a6b77be123
unknown <mohsenomar350@gmail.com>
2022-04-23 02:08:21 -0700

    omar

--- App/Routes/auth.py
+++ App/Routes/auth.py
@@ -10,14 +10,14 @@ from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from fastapi.encoders import jsonable_encoder
 from App.security.Oauth import get_current_user, get_current_admin
-router = APIRouter(
+routerauth = APIRouter(
     prefix="/auth",
     tags=['auth']
 )
 db = get_db
 
 
-@router.get('/')
+@routerauth.get('/')
 def get_user(db: Session = Depends(db)):
     json_compatible_item_data = jsonable_encoder(
         list(map(lambda e: schemas.User(**dict(e.__dict__), Role=e.Roles.Name), UserCrud.get_all(db))))
@@ -25,31 +25,31 @@ def get_user(db: Session = Depends(db)):
     return JSONResponse(content=json_compatible_item_data)
 
 
-@router.post('/create-account')
+@routerauth.post('/create-account')
 def createAccount(request: schemas.CreateAccount, db: Session = Depends(db)):
     return create(request, db)
 
 
-@router.post('/login')
+@routerauth.post('/login')
 def Login(request: schemas.LoginForm, db: Session = Depends(db)):
     return login(request, db)
 
 
-@router.get('/admin')
+@routerauth.get('/admin')
 def Login(db: Session = Depends(db), current_admin: schemas.User = Depends(get_current_admin)):
     return "admin"
 
 
-@router.get('/user')
+@routerauth.get('/user')
 def Login(db: Session = Depends(db), current_admin: schemas.User = Depends(get_current_user)):
     return "user"
 
 
-@router.delete('/{id}')
+@routerauth.delete('/{id}')
 def createAccount(id: int, db: Session = Depends(db)):
     return UserCrud.delete(db, id)
 
 
-@router.delete('/all/')
+@routerauth.delete('/all/')
 def createAccount(db: Session = Depends(db)):
     return UserCrud.delete_all(db)
--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from App.routes import auth, item, role
+from App.routes.auth import routerauth
 from fastapi.testclient import TestClient
 from fastapi.staticfiles import StaticFiles
 # sqlalchemy uvicorn alembic fastapi pyodbc python-dotenv
@@ -16,9 +16,7 @@ from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
-app.include_router(auth.router)
-app.include_router(item.router)
-app.include_router(role.router)
+app.include_router(routerauth)
 
 app.add_middleware(
     CORSMiddleware,
