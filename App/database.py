55e75d04b52b9e17fa79c12fa9d9748528d5480c
unknown <mohsenomar350@gmail.com>
2022-04-23 14:29:43 -0700

    imar

--- App/database.py
+++ App/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 from dotenv import dotenv_values
 
 engine = create_engine(
-    dotenv_values("pyvenv.cfg")['Db']
+    'mssql+pyodbc://omar:Om123456@92.204.138.94:53956/Fast_Api_Test?driver=SQL+Server'
 )
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False,)
 Base = declarative_base()
