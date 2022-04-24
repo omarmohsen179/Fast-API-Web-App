f13afd0e7887b2fd006fc26911b79f4bd81fcde4
unknown <mohsenomar350@gmail.com>
2022-04-23 19:39:43 -0700

    omar

--- App/database.py
+++ App/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, MetaData, Column, Integer, String, Boolean, Table
 from sqlalchemy.orm import declarative_base, sessionmaker
-from dotenv import dotenv_values
+#from dotenv import dotenv_values
 import urllib
 
 Base = declarative_base()
@@ -13,14 +13,7 @@ notes = Table(
     Column("text", String),
     Column("completed", Boolean),
 )
-params = urllib.parse.quote_plus("Driver={ODBC Driver 17 for SQL Server}"
-                                 "Server=tcp:subue1.database.windows.net,1433;"
-                                 "DATABASE=FastApi2;"
-                                 "Uid=omar;"
-                                 "Pwd=Mrj8P8kSSUGDqNRP1e;"
-                                 "Trusted_Connection = yes")
 engine = create_engine(
-
     "mssql+pyodbc://omar:Mrj8P8kSSUGDqNRP1e@subue1.database.windows.net:1433/FastApi2?driver=SQL+Server"
     # "jdbc:sqlserver://subue1.database.windows.net:1433;database=FastApi2;user=omar@subue1;password=Mrj8P8kSSUGDqNRP1e;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;"
 )
