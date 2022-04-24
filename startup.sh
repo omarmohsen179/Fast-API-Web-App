2cf9bd39806cb4e1fe955939f094980b0589bab4
unknown <mohsenomar350@gmail.com>
2022-04-24 03:13:29 -0700

    omar

--- startup.sh
+++ startup.sh
@@ -3,5 +3,6 @@ apt-get update
 apt-get install g++
 apt-get install unixodbc-dev
 apt-get install mssql-tools unixodbc-dev -y
+Yes
 apt-get install unixodbc unixodbc-dev freetds-dev freetds-bin tdsodbc
 gunicorn -w 4 -k uvicorn.workers.UvicornWorker App.main:app
