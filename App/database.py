fe0eb59c3384b26484213098abb06d515af096ab
unknown <mohsenomar350@gmail.com>
2022-04-24 02:47:39 -0700

    omR

--- App/database.py
+++ App/database.py
@@ -1,3 +1,4 @@
+import pyodbc
 from sqlalchemy import create_engine, MetaData, Column, Integer, String, Boolean, Table
 from sqlalchemy.orm import declarative_base, sessionmaker
 #from dotenv import dotenv_values
@@ -11,6 +12,10 @@ notes = Table(
     Column("text", String),
     Column("completed", Boolean),
 )
+
+cnxn = pyodbc.connect(
+    "Driver={ODBC Driver 13 for SQL Server};Server=tcp:subue1.database.windows.net,1433;Database=FastApi2;Uid=omar;Pwd=Mrj8P8kSSUGDqNRP1e;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
+cursor = cnxn.cursor()
 engine = create_engine(
     "mssql+pyodbc://omar:Mrj8P8kSSUGDqNRP1e@subue1.database.windows.net:1433/FastApi2?driver=SQL+Server"
     # "jdbc:sqlserver://subue1.database.windows.net:1433;database=FastApi2;user=omar@subue1;password=Mrj8P8kSSUGDqNRP1e;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;"
